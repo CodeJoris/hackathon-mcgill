f52b6c8ccc8c7762bf0e09c053c1e339276ac6f1
marekdetven <marekvenkatesh@hotmail.com>
2024-11-16 08:17:31 -0500

    changes to Mass

---
+++ Mass.py
@@ -0,0 +1,14 @@
+import Vector as v
+
+class Mass:
+  def __init__(self, name, radius, mass, x_pos, y_pos, velocity, color):
+    self.name=name
+    self.radius=radius
+    self.mass=mass
+    self.pos=v.Vector(x_pos,y_pos)
+    self.velocity=velocity
+    self.color=color
+  def get_position(self):
+    return self.pos
+  def get_mass(self):
+    return self.mass
