2dfdda5b751f2b21b3fd66ada574c216af3d2ff5
LAPTOP-777\theil <joristheil.ecole@gmail.com>
2024-11-16 11:16:56 -0500

    massive update lads

--- Mass.py
+++ Mass.py
@@ -1,42 +1,59 @@
-import Vector as v
+
 from math import *
 import pygame
+import numpy as np
+
+def norm(array):
+    norm=(array[0]**2+array[1]**2)**0.5
+    return norm
+
+def unit_vector(array):
+  return array / norm(array)
+
+def polar_angle(array):
+  return atan((-array[1])/array[0]) 
+  
+def pixels_to_meters(array):
+  return array*(1.5*10**11/300)
 
 class Mass:
   def __init__(self, name, radius, mass, x_pos, y_pos, velocity, color):
     self.name=name
     self.radius=radius
     self.mass=mass
-    self.pos=v.Vector(x_pos,y_pos)
-    self.velocity=v.Vector(velocity[0],velocity[1])
+    self.pos=np.array([x_pos,y_pos])
+    self.velocity=np.array([velocity[0],velocity[1]],dtype=np.float64)
     self.color=color
 
   def get_position(self):
+    '''return np.array'''
     return self.pos
   
   def get_mass(self):
     return self.mass
   
   def pygame_position(self):
-    return (self.pos.vec[0],self.pos.vec[1])
+    return (self.pos[0],self.pos[1])
   
   def acceleration_due_to(self,other):
-    
     G=6.67*10**-11
-    relative_pos = v.Vector(self.get_position()-other.get_position()) #position vector between Sun and Earthin Pixels
-    relative_pos_meters = relative_pos.pixels_to_meters()
-    distance = relative_pos_meters.norm()#distance between Earth and Sun in pixels
-    theta = relative_pos.polar_angle()
+    relative_pos = self.get_position() - other.get_position() #position vector between Sun and Earthin Pixels
+    relative_pos_meters = pixels_to_meters(relative_pos)
+    distance = norm(relative_pos_meters)#distance between Earth and Sun in pixels
+    theta = polar_angle(relative_pos)
+    factor=1
+    if theta > pi/2:
+       factor=-1
     norm_acceleration = -G*other.get_mass()/(distance**2)
-    acceleration = v.Vector(norm_acceleration*cos(theta),norm_acceleration*sin(theta))
-    return acceleration
+    acceleration = np.array([norm_acceleration*cos(theta),norm_acceleration*sin(theta)])
+    return acceleration * factor
 
   def update_position(self):
       self.pos += self.velocity
 
-  def apply_force(self, force):
-      acceleration = force / self.mass
-      self.velocity += acceleration
+  def apply_acceleration_due_to(self,other):
+
+      self.velocity += self.acceleration_due_to(other)
 
   def draw(self, screen):
       center = (int(self.pos[0]), int(self.pos[1]))
--- main.py
+++ main.py
@@ -6,8 +6,9 @@ from random import randint, choice
 
 
 import Mass as m
-import Vector as v
 from math import cos,sin 
+
+
 # >>>>>>> cb61859c766c4646f5cde64f6ff4895c0f046f99
 os.environ['SDL_VIDEO_WINDOW_POS'] = "%d,%d" % (10,30)
 #Configuration
@@ -18,12 +19,12 @@ VITESSE=10
 
 screen = pygame.display.set_mode(DIMENSIONS)
 rafraichissement = pygame.time.Clock()
-G=6.67*10**-11
+G=6.67*10**-11  
 BACKGROUND = (0,0,0)
 pygame.init()
 ### Definition of our two masses using class Mass ###
-sun = m.Mass("Sun",6.957*10,1.989*10**30,WIDTH/2,HEIGHT/2,0,(255,255,0))
-earth = m.Mass("Earth",2*6.378,5.9722*10**24,(WIDTH/2)+300,HEIGHT/2,np.array([0,1]),(0,0,255))
+sun = m.Mass("Sun",6.957*10,1.989*10**30,WIDTH/2,HEIGHT/2,(0,0),(255,255,0))
+earth = m.Mass("Earth",2*6.378,5.9722*10**24,(WIDTH/2)+300,HEIGHT/2,(0,-1),(0,0,255))
 
 
 
@@ -38,6 +39,7 @@ while running:
 
     # Draw a sun
     earth.update_position()
+    earth.apply_acceleration_due_to(sun)
     pygame.draw.circle(screen, sun.color, sun.pygame_position(), sun.radius)
     pygame.draw.circle(screen, earth.color, earth.pygame_position(), earth.radius)
 
