7efaba47f428471bffac74cefa54f8867e71ad3e
LAPTOP-777\theil <joristheil.ecole@gmail.com>
2024-11-16 08:10:17 -0500

    Added vector class

---
+++ Vector.py
@@ -0,0 +1,8 @@
+class Vector:
+  def __init__(self,x_coord,y_coord):
+    self.x_coord=x_coord
+    self.y_coord=y_coord
+
+  def norm(self):
+     norm=(self.x_coord**2+self.y_coord**2)**0.5
+     return norm
