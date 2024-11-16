0069eefa91d760b589777851d232f9291a0070e6
Kieran Tan <kieranbieran@icloud.com>
2024-11-16 10:03:48 -0500

    i made the planets look like somewhat reasonable

--- main.py
+++ main.py
@@ -12,19 +12,18 @@ from math import cos,sin
 os.environ['SDL_VIDEO_WINDOW_POS'] = "%d,%d" % (10,30)
 #Configuration
 DIMENSIONS = WIDTH, HEIGHT =(1000,1000)
-BACKGROUND = (0,0,0)
 CENTER=(DIMENSIONS[0]/2,DIMENSIONS[1]/2)
 
 VITESSE=10
 
 screen = pygame.display.set_mode(DIMENSIONS)
 rafraichissement = pygame.time.Clock()
-
+G=6.67*10**-11
 BACKGROUND = (0,0,0)
-
+pygame.init()
 ### Definition of our two masses using class Mass ###
-sun = m.Mass("Sun",6.957*10**8,1.989*10**30,WIDTH/2,HEIGHT/2,0,(255,255,255))
-earth = m.Mass("Earth",6.378*10**6,5.9722*10**24,(WIDTH/2)+300,HEIGHT/2,np.array([0,1]),(255,255,255))
+sun = m.Mass("Sun",6.957*10,1.989*10**30,WIDTH/2,HEIGHT/2,0,(255,255,0))
+earth = m.Mass("Earth",2*6.378,5.9722*10**24,(WIDTH/2)+300,HEIGHT/2,np.array([0,1]),(0,0,255))
 
 def acceleration_update():
     relative_pos = v.Vector(earth.get_position()-sun.get_position()) #position vector between Sun and Earthin Pixels
@@ -34,6 +33,7 @@ def acceleration_update():
     norm_acceleration = -G*sun.get_mass()/(distance**2)
     acceleration = v.Vector(norm_acceleration*cos(theta),norm_acceleration*sin(theta))
     return acceleration
+
 
 running = True
 while running:
@@ -48,8 +48,9 @@ while running:
     pygame.draw.circle(screen, sun.color, sun.pygame_position(), sun.radius)
     pygame.draw.circle(screen, earth.color, earth.pygame_position(), earth.radius)
 
-    # Update the display
+   # Update the display
     pygame.display.flip()
+    rafraichissement.tick(60)
 
 # Quit Pygame
 pygame.quit()
