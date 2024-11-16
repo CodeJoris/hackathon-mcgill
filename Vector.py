85482da2d1fd63f9826985c0d56c5cc1136f2698
LAPTOP-777\theil <joristheil.ecole@gmail.com>
2024-11-16 08:17:52 -0500

    Made vector an numpy array

--- Vector.py
+++ Vector.py
@@ -1,8 +1,14 @@
+import numpy as np
+
 class Vector:
   def __init__(self,x_coord,y_coord):
+    self.vec = np.array([x_coord,y_coord])
     self.x_coord=x_coord
     self.y_coord=y_coord
 
   def norm(self):
      norm=(self.x_coord**2+self.y_coord**2)**0.5
      return norm
+  
+  def unit_vector(self):
+    return self.vec 
