2a5072ebbfcf60919ed5afa42d1bd3094d6e96d2
marekdetven <marekvenkatesh@hotmail.com>
2024-11-17 08:23:56 -0500

    heart image

--- marek_work_in_progress.py
+++ marek_work_in_progress.py
@@ -1,7 +1,9 @@
-def lives_hearts(lives):
-    lives_image = pygame.image.load('sun.png').convert_alpha()
-    lives_size = (50,50)
-    lives_image = pygame.transform.scale(lives_image,lives_size)
-    lives_image.set_colorkey((255,255,255))
+pygame,screen = 0
+
+lives_image = pygame.image.load('sun.png').convert_alpha()
+lives_size = (50,50)
+lives_image = pygame.transform.scale(lives_image,lives_size)
+lives_image.set_colorkey((255,255,255))
+def lives_hearts(lives):    
     for i in range(lives):
         screen.blit(lives_image,(940-60*i,20))
