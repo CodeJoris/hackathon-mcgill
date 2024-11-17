13c89ee2aa4aa4de4e348a70a3d0baf133b667d5
LAPTOP-777\theil <joristheil.ecole@gmail.com>
2024-11-16 21:21:31 -0500

    staged changes

--- main.py
+++ main.py
@@ -100,17 +100,23 @@ box_width, box_height = 300, 40
 fill_width=(fuel_level/max_fuel)*box_width
 
 # Progress bar
-font = pygame.font.Font(None, 36)
-tag_text = f"Fuel Levels : {int(fuel_level)} / {max_fuel}"
-text_surface = font.render(tag_text, True, (255,0,0))
-text_rect = text_surface.get_rect(center=(box_x + box_width // 2, box_y - 20))  # Position above the box
-
 progress=0
 max_progress=100
-progress_box_x, progress_box_y = WIDTH//2, HEIGHT-50
-progress_box_width, progress_box_height = 300, 40
-progress_fill_width=(progress/max_progress)*progress_box_width
+fuel_box_x, fuel_box_y = WIDTH//2, HEIGHT-50
+fuel_box_width, fuel_box_height = 300, 40
+fuel_fill_width=(progress/max_progress)*fuel_box_width
 
+#fuel text
+font = pygame.font.Font(None, 36)
+fuel_tag_text = f"Fuel Levels : {int(fuel_level)} / {max_fuel}"
+fuel_text_surface = font.render(fuel_tag_text, True, (255,0,0))
+fuel_text = fuel_text_surface.get_rect(center=(fuel_box_x + fuel_box_width // 2, fuel_box_y - 20))  # Position above the box
+
+#progress text
+font = pygame.font.Font(None, 36)
+progress_tag_text = f"Progress : {int(progress)} / {max_progress}"
+progress_text_surface = font.render(progress_tag_text, True, (255,0,0))
+progress_text = progress_text_surface.get_rect(center=(box_x + box_width // 2, box_y - 20))  # Position above the box
 
 
 # Add masses to group
@@ -225,14 +231,25 @@ while running:
     # velocity earth
     if earth.norm_velocity() > 2:
         progress += 0.026
-    progress_fill_width=(progress/max_progress)*progress_box_width
+    fuel_fill_width=(progress/max_progress)*fuel_box_width
 
     all_sprites.update()
 
-    #draw progress box
-    screen.blit(text_surface, text_rect)
-    pygame.draw.rect(screen, (90, 102, 92), (progress_box_x, progress_box_y, progress_box_width, progress_box_height), 2)
-    pygame.draw.rect(screen, (222, 173, 45), (progress_box_x, progress_box_y, progress_fill_width, progress_box_height))
+    #draw fuel text
+    fuel_text = f"Fuel Levels : {int(fuel_level)} / {max_fuel}"
+    fuel_text_surface = font.render(fuel_text, True, (255,0,0))
+    fuel_text_pos = (box_x, box_y - 40)
+    screen.blit(fuel_text_surface, fuel_text_pos)
+
+    #draw progress text
+    progress_text = f"Progress Level : {int(progress)} / {max_progress}"
+    progress_text_surface = font.render(progress_text, True, (255,255,0))
+    progress_text_pos = (fuel_box_x, fuel_box_y - 40)
+    screen.blit(progress_text_surface, progress_text_pos)
+
+    #draw fuel rectable
+    pygame.draw.rect(screen, (90, 102, 92), (fuel_box_x, fuel_box_y, fuel_box_width, fuel_box_height), 2)
+    pygame.draw.rect(screen, (222, 173, 45), (fuel_box_x, fuel_box_y, fuel_fill_width, fuel_box_height))
         
     #draw rectangles fuel box
     pygame.draw.rect(screen, (200,200,200), (box_x, box_y, box_width, box_height), 2)
